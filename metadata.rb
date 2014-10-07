43fe5369f26b61aa365499cb81c151f6b66325a9
Daniel Ku <kjunine@gmail.com>
2014-10-07 16:16:05 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'docker-redis'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-redis'
+long_description 'Installs/Configures docker-redis'
+version          '0.1.0'
+
